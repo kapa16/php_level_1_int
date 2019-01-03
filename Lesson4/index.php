26f0b3a05d9a6e59e0b0b015bc8a58c57d315c89
kapa16 <kapa16@inbox.ru>
2019-01-03 21:57:01 +0500

    task 1 & 2

--- Lesson4/index.php
+++ Lesson4/index.php
@@ -1,7 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kapa
- * Date: 03.01.2019
- * Time: 20:22
- */
+
+echo "1. Создайте функцию is_odd(), которая принимает в качестве аргумента целое число и возвращает true, <br>
+в случае если число нечетное и false — в противном случае.<br><br>";
+function is_odd(int $num) {
+    return $num % 2 !== 0;
+}
+
+for ($i = 1; $i <= 10; $i++) {
+    if (is_odd($i)) {
+        echo "Число $i нечетное <br>";
+    } else {
+        echo "Число $i четное <br>";
+    }
+}
+echo "<hr>";
+
+echo "2. Создайте функцию sum(), которая принимает любое количество числовых аргументов и возвращает их сумму.<br><br>";
+
+function sum(...$args) {
+    $sum = 0;
+    foreach ($args as $arg) {
+        $sum += $arg;
+    }
+    return $sum;
+}
+
+echo sum(4,7,8,3,4);

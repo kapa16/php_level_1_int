729e048258a181507e1bc40c5f1c49320d0c4b12
Pavel <kapa16@inbox.ru>
2019-01-17 10:49:08 +0500

    1

--- Lesson8/task1.php
+++ Lesson8/task1.php
@@ -5,48 +5,50 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Form</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Task2</title>
 </head>
 <body>
-
-<a href="index.php">Домой</a>
-<br>
-<hr>
-
-<form action="#" method="post">
-    <label> Введите координаты 1-ой точки: X
-        <input type="number" name="x1" step="0.01">
-        Y
-        <input type="number" name="y1" step="0.01">
-    </label>
+<div class="container">
+    <a href="index.php">Домой</a>
     <br>
-    <label> Введите координаты 2-ой точки: X
-        <input type="number" name="x2" step="0.01">
-        Y
-        <input type="number" name="y2" step="0.01">
-    </label>
-    <br>
-    <button type="submit">Рассчитать</button>
-    <br>
-</form>
+    <hr>
 
-<?php
-function getValue($index) {
-    if (isset($_POST[$index])) {
-        return (float) $_POST[$index];
-    } else {
-        return 0;
+    <form action="#" method="post">
+        <label> Введите координаты 1-ой точки: X
+            <input type="number" name="x1" step="0.01">
+            Y
+            <input type="number" name="y1" step="0.01">
+        </label>
+        <br>
+        <label> Введите координаты 2-ой точки: X
+            <input type="number" name="x2" step="0.01">
+            Y
+            <input type="number" name="y2" step="0.01">
+        </label>
+        <br>
+        <button type="submit">Рассчитать</button>
+        <br>
+    </form>
+
+    <?php
+    function getValue($index)
+    {
+        if (isset($_POST[$index])) {
+            return (float)$_POST[$index];
+        } else {
+            return 0;
+        }
     }
-}
 
-$x1 = getValue('x1');
-$x2 = getValue('x2');
-$y1 = getValue('y1');
-$y2 = getValue('y2');
+    $x1 = getValue('x1');
+    $x2 = getValue('x2');
+    $y1 = getValue('y1');
+    $y2 = getValue('y2');
 
 
-echo "Расстояние между точками: " . sqrt(pow($x2 - $x1, 2) + pow($y2 - $y1, 2));
-?>
-
+    echo "Расстояние между точками: " . sqrt(pow($x2 - $x1, 2) + pow($y2 - $y1, 2));
+    ?>
+</div>
 </body>
 </html>
--- Lesson8/task2.php
+++ Lesson8/task2.php
@@ -1,7 +1,50 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 17.01.2019
- * Time: 9:39
- */
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>task2</title>
+</head>
+<body>
+<div class="container">
+    <a href="index.php">Домой</a>
+    <hr>
+
+    <form action="#" method="post">
+        <label>
+            Введите количество товаров:
+            <input type="text" name="quantity" id="quantity">
+        </label>
+        <br>
+        <label>
+            Введите цену товара:
+            <input type="text" name="price" id="price">
+        </label>
+        <br>
+        <br>
+        <button type="submit">Рассчитать сумму корзины</button>
+    </form>
+
+    <?php
+    function getValue($index)
+    {
+        if (isset($_POST[$index])) {
+            return (float)$_POST[$index];
+        } else {
+            return 0;
+        }
+    }
+
+    $quantity = getValue('quantity');
+    $price = getValue('price');
+
+    echo 'Сумма = ' . ($quantity * $price);
+
+
+    ?>
+</div>
+</body>
+</html>
